46d719e9b6d6d1bb4411b24a66fa05cb1ff7641d
Sangjae Kim <kimsangjae94114@gmail.com>
2025-11-21 15:36:00 +0900

    fix: 유저 엔드포인트 /users/user 추가

--- Linkee-api/src/main/java/com/linkee/linkeeapi/users/query/controller/UserQueryController.java
+++ Linkee-api/src/main/java/com/linkee/linkeeapi/users/query/controller/UserQueryController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/users")
+@RequestMapping("/api/v1/users/user")
 @Tag(name = "회원", description = "회원가입, 로그인, 계정, 프로필 관련 API")
 public class UserQueryController {
 
