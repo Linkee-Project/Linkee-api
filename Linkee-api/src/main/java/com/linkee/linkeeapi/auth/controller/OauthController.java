794da632c309a2880c83eaf72566a3a0aa5c68b8
yendlessse <yoohanse@naver.com>
2025-11-21 16:19:22 +0900

    fix : oauth 수정 완료

--- Linkee-api/src/main/java/com/linkee/linkeeapi/auth/controller/OauthController.java
+++ Linkee-api/src/main/java/com/linkee/linkeeapi/auth/controller/OauthController.java
@@ -59,7 +59,7 @@ public class OauthController {
 
 
     // ✅ Redis에 저장된 RefreshToken 삭제 (소셜로그인 로그아웃)
-    @PostMapping("/logout") public ResponseEntity<String> logout(@RequestParam String email) {
+    @PostMapping("/api/v1/oauth/logout") public ResponseEntity<String> logout(@RequestParam String email) {
         authRedisRepository.delete(email); // Redis에서 RefreshToken 삭제
         return ResponseEntity.ok("로그아웃 완료");
     }
